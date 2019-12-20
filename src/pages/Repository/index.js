434dde2ce284a9ed98a5975963c153ca7f3ece7d
Evaldo Klock <neto.nemesis@gmail.com>
2019-12-20 15:37:06 -0400

    Implementados a busca e a adição de repositórios, em memória, do github na lista

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title } from './styles';
+
+// import { Container } from './styles';
+
+export default function Repository() {
+  return (
+    <Title>
+      <h1>Repository</h1>
+    </Title>
+  );
+}
---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+export { Title };
